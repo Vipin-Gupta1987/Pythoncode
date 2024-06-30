9965801a610d715ddc4da7788d32fc5e34d55b56
Vipin-Gupta1987 <93309152+Vipin-Gupta1987@users.noreply.github.com>
2024-06-30 16:30:32 +0530

    Update test_git.py

--- test_git.py
+++ test_git.py
@@ -1,4 +1,4 @@
 print("Hello Buddy")
 print("Edited the code just for verification")
-print("another addition to check for github pollscm")
+print("another addition to check for github")
 print("Sum of two number is ", 10 + 20)
